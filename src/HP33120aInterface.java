62586d74eab9ca7293c358ebd7536e5e8d4b35b3
Alvaro Pascual <varopascual@gmail.com>
2013-09-10 19:48:02 +0200

    Removed fields that are not used in this device

--- src/HP33120aInterface.java
+++ src/HP33120aInterface.java
@@ -17,19 +17,16 @@ public interface HP33120aInterface {
 	public JComboBox getSignalShape();
 	public JComboBox getModWfmShape();
 	public JComboBox getModType();
+	public JComboBox getUnit();
 	public JButton getConnectButton();
 	public String getBurstPhase();
 	public String getBurstCount();
 	public String getBurstRate();
-	public String getPhaseDeviationPM();
-	public String getIntDeviationPWM();
 	public String getHopFrequency();
 	public String getFmDeviation();
 	public String getAmDepth();
 	public String getModulatingFreq();
-	public String getDutyCyclePulse();
 	public String getDutyCycleSquare();
-	public String getRampSymmetry();
 	public String getAmplitude();
 	public String getFrequency();
 	public String getOffset();
@@ -39,6 +36,9 @@ public interface HP33120aInterface {
 	
 	static final int QUERY_MESSAGE_TYPE = 10;
 
+	static final String VPP = "Vpp";
+	static final String VRMS = "Vrms";
+	static final String DB = "dB";
 	static final String TYPE_OF_SIGNAL = "HP33120a_Type_of_Signal";
 	static final String MODULATION = "Modulation";
 	static final String SIGNAL = "Simple Signal";
@@ -46,15 +46,11 @@ public interface HP33120aInterface {
 	static final String FREQUENCY = "Frequency";
 	static final String AMPLITUDE ="HP33120a_Amplitude";
 	static final String OFFSET = "HP33120a_Offset";
-	static final String RAMP_SYMMETRY = "HP33120a_Ramp_Symmetry";
 	static final String DUTY_CYCLE_SQUARE = "HP33120a_Duty_Cycle_Square";
-	static final String DUTY_CYCLE_PULSE = "HP33120a_Duty_Cycle_Pulse";
 	static final String MODULATING_FREQUENCY = "HP33120a_Modulating_Frequency";
 	static final String AM_DEPTH = "HP33120a_AM_Depth";
 	static final String FM_DEVIATION = "HP33120a_FM_Deviation";
 	static final String HOP_FREQUENCY = "HP33120a_Hop_Frequency";
-	static final String DEVIATION_PWM = "HP33120a_Deviation_PWM";
-	static final String PHASE_DEVIATION_PM = "HP33120a_Phase_Deviation";
 	static final String BURST_RATE = "HP33120a_Burst_Rate";
 	static final String BURST_COUNT = "HP33120a_Burst_Count";
 	static final String BURST_PHASE = "HP33120a_Burst_Phase";
